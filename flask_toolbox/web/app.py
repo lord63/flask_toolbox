2e75342e86875092fd44f4ec65be9d99b65b74cd
lord63 <lord63.j@gmail.com>
2016-01-24 11:18:05 +0800

    Add the model view outside the create_app

--- flask_toolbox/web/app.py
+++ flask_toolbox/web/app.py
@@ -10,15 +10,16 @@ from flask_toolbox.web.extensions import db, admin
 from flask_toolbox.web.models import Category, Package, PyPI
 
 
+admin.add_view(sqla.ModelView(Category, db.session))
+admin.add_view(sqla.ModelView(Package, db.session))
+admin.add_view(sqla.ModelView(PyPI, db.session))
+
+
 def create_app(config):
     app = Flask('flask_toolbox')
     app.config.from_object(config)
 
     db.init_app(app)
-
-    admin.add_view(sqla.ModelView(Category, db.session))
-    admin.add_view(sqla.ModelView(Package, db.session))
-    admin.add_view(sqla.ModelView(PyPI, db.session))
     admin.init_app(app)
 
     return app
