9ecb55f254f078bcf8a949640a8a5008f19580fc
lord63 <lord63.j@gmail.com>
2016-01-24 21:26:28 +0800

    Setup admin view for github; init GithubMeta class

--- flask_toolbox/web/app.py
+++ flask_toolbox/web/app.py
@@ -7,12 +7,13 @@ from flask import Flask
 from flask_admin.contrib import sqla
 
 from flask_toolbox.web.extensions import db, admin
-from flask_toolbox.web.models import Category, Package, PyPI
+from flask_toolbox.web.models import Category, Package, PyPI, Github
 
 
 admin.add_view(sqla.ModelView(Category, db.session))
 admin.add_view(sqla.ModelView(Package, db.session))
 admin.add_view(sqla.ModelView(PyPI, db.session))
+admin.add_view(sqla.ModelView(Github, db.session))
 
 
 def create_app(config):
