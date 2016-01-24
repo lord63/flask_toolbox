9e7af5b54b35f37a89c0845da077d9efa9be55fc
lord63 <lord63.j@gmail.com>
2016-01-24 10:29:28 +0800

    Move the database config to the basic Config

--- flask_toolbox/web/configs.py
+++ flask_toolbox/web/configs.py
@@ -9,13 +9,13 @@ import os
 class Config(object):
     SECRET_KEY = '123456790'  # Create dummy secrey key so we can use sessions
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ROOT = os.path.dirname(os.path.realpath(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(
+        os.path.join(ROOT, 'flask_toolbox_dev.db'))
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    ROOT = os.path.dirname(os.path.realpath(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(
-        os.path.join(ROOT, 'flask_toolbox_dev.db'))
 
 
 class ProductionConfig(Config):
