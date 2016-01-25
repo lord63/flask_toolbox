0110b330822fff3fef22bcf038482ff4e7495ed7
lord63 <lord63.j@gmail.com>
2016-01-25 21:47:05 +0800

    We can get the first commit of a github repo now

--- flask_toolbox/crawler/github.py
+++ flask_toolbox/crawler/github.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
+from __future__ import absolute_import, division
 
 import datetime
+import math
 
 from lxml import html
+import requests
 
 
 class GithubMeta(object):
@@ -33,9 +35,23 @@ class GithubMeta(object):
         return self._get_num('.text-emphasized', -1)
 
     @property
+    def commits(self):
+        return self._get_num('.text-emphasized', 0)
+
+    @property
     def issues(self):
         return self._get_num('.counter', 0)
 
     @property
     def pull_requests(self):
         return self._get_num('.counter', -1)
+
+
+def get_first_commit(url):
+    # Steal this idea from https://github.com/wong2/first-commit.
+    commits = GithubMeta(requests.get(url)).commits
+    first_commit_page = int(math.ceil(commits / 35))
+    page_url = "{0}/commits?page={1}".format(url, first_commit_page)
+    tree = html.fromstring(requests.get(page_url).text)
+    first_commit_time = tree.cssselect('time')[-1].get('datetime')
+    return datetime.datetime.strptime(first_commit_time, '%Y-%m-%dT%H:%M:%SZ')
