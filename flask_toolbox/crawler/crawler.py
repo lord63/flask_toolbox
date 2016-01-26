82e31de6253f2ef2834eff67f160be9506a5b70a
lord63 <lord63.j@gmail.com>
2016-01-26 10:28:14 +0800

    We can update the package github info now

--- flask_toolbox/crawler/crawler.py
+++ flask_toolbox/crawler/crawler.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 import requests
 
 from flask_toolbox.crawler.pypi import PyPIMeta
+from flask_toolbox.crawler.github import GithubMeta
 
 
 class Crawler(object):
@@ -14,5 +15,7 @@ class Crawler(object):
         package_info = PyPIMeta(response.json())
         return package_info
 
-    def get_github_info(self):
-        pass
+    def get_github_info(self, url):
+        response = requests.get(url)
+        repo_info = GithubMeta(response)
+        return repo_info
--- flask_toolbox/crawler/worker.py
+++ flask_toolbox/crawler/worker.py
@@ -8,6 +8,8 @@ from flask_toolbox.web.configs import Config
 from flask_toolbox.web.extensions import db
 from flask_toolbox.web.models import PyPI, Github, Package
 from flask_toolbox.crawler.crawler import Crawler
+from flask_toolbox.crawler.github import (get_first_commit,
+                                          get_development_activity)
 
 
 class Worker(object):
@@ -42,4 +44,32 @@ class Worker(object):
                 db.session.commit()
 
     def update_package_github_info(self):
-        pass
+        for package in Package.query.all():
+            repo_info = Crawler().get_github_info(package.source_code_url)
+            github = Github.query.filter_by(package_id=package.id).first()
+            if github:
+                github.watchers = repo_info.watchers
+                github.forks = repo_info.forks
+                github.last_commit = repo_info.last_commit
+                github.contributors = repo_info.contributors
+                github.issues = repo_info.issues
+                github.pull_requests = repo_info.pull_requests
+                github.development_activity = get_development_activity(
+                    package.source_code_url)
+                db.session.add(github)
+                db.session.commit()
+            else:
+                new_github = Github(
+                    package_id = package.id,
+                    watchers = repo_info.watchers,
+                    forks = repo_info.forks,
+                    last_commit = repo_info.last_commit,
+                    contributors = repo_info.contributors,
+                    issues = repo_info.issues,
+                    pull_requests = repo_info.pull_requests,
+                    development_activity = get_development_activity(
+                        package.source_code_url),
+                    first_commit = get_first_commit(package.source_code_url)
+                )
+                db.session.add(new_github)
+                db.session.commit()
