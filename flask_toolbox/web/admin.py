47c8810325e29251c3faf8d426b08352eff50e66
lord63 <lord63.j@gmail.com>
2016-01-31 09:59:44 +0800

    Add search for package and category admin view

---
+++ flask_toolbox/web/admin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from flask_admin.contrib.sqla import ModelView
+
+
+class CategoryView(ModelView):
+    column_searchable_list = ['name']
+
+
+class PackageView(ModelView):
+    column_searchable_list = ['name']
--- flask_toolbox/web/app.py
+++ flask_toolbox/web/app.py
@@ -4,16 +4,18 @@
 from __future__ import absolute_import
 
 from flask import Flask
-from flask_admin.contrib import sqla
+
+from flask_admin.contrib.sqla import ModelView
 
 from flask_toolbox.web.extensions import db, admin
 from flask_toolbox.web.models import Category, Package, PyPI, Github
+from flask_toolbox.web.admin import CategoryView, PackageView
 
 
-admin.add_view(sqla.ModelView(Category, db.session))
-admin.add_view(sqla.ModelView(Package, db.session))
-admin.add_view(sqla.ModelView(PyPI, db.session))
-admin.add_view(sqla.ModelView(Github, db.session))
+admin.add_view(CategoryView(Category, db.session))
+admin.add_view(PackageView(Package, db.session))
+admin.add_view(ModelView(PyPI, db.session))
+admin.add_view(ModelView(Github, db.session))
 
 
 def create_app(config):
