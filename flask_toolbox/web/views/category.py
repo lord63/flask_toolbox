72252654a9f1fee09e1390eaee9d39e6ae50cf11
lord63 <lord63.j@gmail.com>
2016-02-09 14:13:47 +0800

    Add the basic category page

--- flask_toolbox/web/views/category.py
+++ flask_toolbox/web/views/category.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
+
+from flask import Blueprint, render_template
+
+
+category_page = Blueprint('category_page', __name__,
+                         template_folder='templates')
+
+
+@category_page.route('/categories/<category>')
+def index(category):
+    return render_template('category.html')
