e0285becc08f49f75531951e873cc6f91a27591d
lord63 <lord63.j@gmail.com>
2016-02-09 16:25:45 +0800

    Read package info from the database
    * package name
    * package description
    * other related packages in the same category

--- flask_toolbox/web/views/package.py
+++ flask_toolbox/web/views/package.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 
 from flask import Blueprint, render_template
 
+from flask_toolbox.web.models import Package
+
 
 package_page = Blueprint('package_page', __name__,
                          template_folder='templates')
@@ -12,4 +14,13 @@ package_page = Blueprint('package_page', __name__,
 
 @package_page.route('/packages/<package>')
 def index(package):
-    return render_template('package.html')
+    the_package = Package.query.filter_by(name=package).first_or_404()
+    category = the_package.category
+    related_packages = [item.name for item in category.packages.all()
+                        if item.name != package]
+    sidebar_title = (
+        "Other related packages in the same category".format(category.name)
+    )
+    return render_template(
+        'package.html', package=the_package,
+        related_packages=related_packages, sidebar_title=sidebar_title)
