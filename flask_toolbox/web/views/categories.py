85ac5f9a46a532f90102102104fcb00caa5826a4
lord63 <lord63.j@gmail.com>
2016-02-11 10:36:17 +0800

    Add the simple categories page

--- flask_toolbox/web/views/categories.py
+++ flask_toolbox/web/views/categories.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 
 from flask import Blueprint, render_template
 
+from flask_toolbox.web.models import Category
+
 
 categories_page = Blueprint('categories_page', __name__,
                             template_folder='templates')
@@ -12,4 +14,9 @@ categories_page = Blueprint('categories_page', __name__,
 
 @categories_page.route('/categories')
 def index():
-    return render_template('categories.html')
+    categories = Category.query.order_by(Category.name).all()
+    sidebar_title = "All the categories"
+    category_list = [category.name for category in categories]
+    return render_template(
+        'categories.html', categories=categories,
+         sidebar_title=sidebar_title, category_list=category_list)
