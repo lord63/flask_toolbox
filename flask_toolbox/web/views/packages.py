a3c48e5e25902e6319781c552e7f3a8e56971979
lord63 <lord63.j@gmail.com>
2016-02-11 16:32:10 +0800

    We have a basic packages page

--- flask_toolbox/web/views/packages.py
+++ flask_toolbox/web/views/packages.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 
 from flask import Blueprint, render_template
 
+from flask_toolbox.web.models import Package
+
 
 packages_page = Blueprint('packages_page', __name__,
                           template_folder='templates')
@@ -12,4 +14,10 @@ packages_page = Blueprint('packages_page', __name__,
 
 @packages_page.route('/packages')
 def index():
-    return render_template('packages.html')
+    packages = Package.query.order_by(Package.name).all()
+    sidebar_title = "All the packages"
+    package_list = [package.name for package in packages]
+    print(len(package_list))
+    return render_template(
+        'packages.html', packages=packages,
+        sidebar_title=sidebar_title, package_list=package_list)
