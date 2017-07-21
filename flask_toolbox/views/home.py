29c8f09630bc1bfef700d77579f134e49f665329
lord63 <lord63.j@gmail.com>
2017-07-21 08:54:45 +0800

    Support simple search, close #28

--- flask_toolbox/views/home.py
+++ flask_toolbox/views/home.py
@@ -3,8 +3,10 @@
 
 from __future__ import absolute_import
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from sqlalchemy import or_
 
+from flask_toolbox.models import Package
 
 
 home_page = Blueprint('home_page', __name__,
@@ -19,3 +21,15 @@ def index():
 @home_page.route('/intro')
 def intro():
     return render_template('intro.html')
+
+
+@home_page.route('/search', methods=['POST'])
+def search():
+    keywords = filter(lambda x: len(x) > 0, request.form['keywords'].split(' '))
+    if not len(keywords):
+        return redirect(url_for('home_page.index'))
+    name_query = [Package.name.ilike('%{0}%'.format(word)) for word in keywords]
+    description_query = [Package.description.ilike('%{0}%'.format(word)) for word in keywords]
+    query = name_query + description_query
+    packages = Package.query.filter(or_(*query)).all()
+    return render_template('search.html', packages=packages, keywords=request.form['keywords'] )
