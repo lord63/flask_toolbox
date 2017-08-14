c8fa6f7d6160c5b4c69a8679748ad4750655c991
lord63 <lord63.j@gmail.com>
2017-08-14 23:03:16 +0800

    Add the pacakge score page

--- flask_toolbox/views/package.py
+++ flask_toolbox/views/package.py
@@ -38,3 +38,21 @@ def show(package):
     return render_template(
         'package.html', package=the_package,
         related_packages=related_packages, sidebar_title=sidebar_title)
+
+
+@package_page.route('/packages/<package>/score')
+def score(package):
+    flask = Package.query.filter_by(name="Flask").first()
+    the_package = Package.query.filter_by(name=package).first_or_404()
+    category = the_package.category
+    related_packages = [item.name for item in category.packages.order_by(Package.score.desc()).all()
+                        if item.name != package]
+    sidebar_title = (
+        Markup("Other related packages in the <a href='{0}'>{1}</a> category".format(
+             url_for('category_page.index', category=category.name),
+             category.name
+        ))
+    )
+    return render_template(
+        'score.html', package=the_package, flask=flask,
+        related_packages=related_packages, sidebar_title=sidebar_title)
